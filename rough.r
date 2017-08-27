656ec1db3073068178b5ce0d5aa478c394fc3379
mooromets <sergey.sambor@gmail.com>
2017-08-27 23:19:37 +0200

    check the most frequent words from dictionary

---
+++ rough.r
@@ -0,0 +1,29 @@
+library("RWeka")
+library("tm")
+library("SnowballC")
+
+# at first read dictionary
+dict <- VCorpus(DirSource("./dictionary/"))
+dict <- tm_map(dict, stripWhitespace)
+dict <- tm_map(dict, removePunctuation)
+dict <- tm_map(dict, removeNumbers)
+dict <- tm_map(dict, stemDocument)
+dict <- tm_map(dict, content_transformer(tolower))
+
+jobs <- VCorpus(DirSource("./job/"))
+
+jobs <- tm_map(jobs, stripWhitespace)
+jobs <- tm_map(jobs, removePunctuation)
+jobs <- tm_map(jobs, removeNumbers)
+jobs <- tm_map(jobs, stemDocument)
+jobs <- tm_map(jobs, content_transformer(tolower))
+
+nGramTok <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 3))
+tdm <- TermDocumentMatrix(jobs, list(#dictionary = dict[[1]]$content,
+                                     tokenize = nGramTok,
+                                     stopwords = FALSE))
+
+findFreqTerms(tdm, 3)
+
+findMostFreqTerms(tdm)
+inspect(tdm)
