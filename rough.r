e2fc64fb145469550b8afde7a684a0403384f0b9
mooromets <sergey.sambor@gmail.com>
2017-08-28 11:56:44 +0200

    switching to data.frame source for jobs' data

--- rough.r
+++ rough.r
@@ -2,32 +2,26 @@ library("RWeka")
 library("tm")
 library("SnowballC")
 
-# at first read dictionary
-#dict <- VCorpus(DirSource("./dictionary/"))
-#dict <- tm_map(dict, stripWhitespace)
-#dict <- tm_map(dict, removePunctuation)
-#dict <- tm_map(dict, removeNumbers)
-#dict <- tm_map(dict, stemDocument)
-#dict <- tm_map(dict, content_transformer(tolower))
+conn <- file("./job/data.frame/17_jobs.txt", open="r")
+jobsData <- readLines(conn)
+close(conn)
 
-# stop words
-stopList <- scan("./stop/stop.txt", what = character(), flush = TRUE)
+jobs <- VCorpus(VectorSource(jobsData))
 
-# jobs
-jobs <- VCorpus(DirSource("./job/"))
 jobs <- tm_map(jobs, stripWhitespace)
 jobs <- tm_map(jobs, removePunctuation)
-jobs <- tm_map(jobs, removeNumbers)
-jobs <- tm_map(jobs, stemDocument)
+#jobs <- tm_map(jobs, removeNumbers)
 jobs <- tm_map(jobs, content_transformer(tolower))
+jobs <- tm_map(jobs, stemDocument)
 
-# mining
-nGramTok <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 3))
-tdm <- TermDocumentMatrix(jobs, list(#dictionary = dict[[1]]$content,
-                                     tokenize = nGramTok,
-                                     stopwords = stopList))
 
-findFreqTerms(tdm, 3)
+nGramTok <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 4))
+termJobMatrix <- 
+  TermDocumentMatrix(jobs, list(tokenize = nGramTok, stopwords = TRUE))
 
-findMostFreqTerms(tdm)
-inspect(tdm)
+
+findFreqTerms(termJobMatrix, 5)
+
+findMostFreqTerms(termJobMatrix)
+
+inspect(termJobMatrix)
